b758e8591bb5ade5a7860991fb1f0e2e7219d2df
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-11-22 16:15:33 +0530

    HeLlo

---
+++ wad/JS MASAI Learn/2.Mathematical Operator.js
@@ -0,0 +1,7 @@
+let a = 3;
+let b = 2;
+console.log("Sum is", a+b);
+console.log("Sub is", a-b);
+console.log("Prod is", a*b);
+console.log("Div is", a/b);
+console.log("Modulo is", a&b);           //remainder
--- wad/problem1.js
+++
@@ -1,7 +0,0 @@
-let name = "Suvrat";
-console.log(name);
- name = "ANIL";
- name = "ANIL Kumar PAndey";
-console.log(name);
- name = "Aparna";
-console.log(name);
