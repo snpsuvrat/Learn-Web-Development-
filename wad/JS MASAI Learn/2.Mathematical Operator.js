ab4dd890fd0f0e47c47746c6623078647a015b68
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-11-22 16:39:12 +0530

    square root

--- wad/JS MASAI Learn/2.Mathematical Operator.js
+++ wad/JS MASAI Learn/2.Mathematical Operator.js
@@ -6,3 +6,9 @@ console.log("Prod is", a*b);
 console.log("Div is", a/b);
 console.log("Modulo is", a&b);           //remainder
 console.log("Exponentiation is", a**b);           //Gives a to the power b
+
+// calculate the square root of 16 using exponentiation operator
+
+let c= 16;
+console.log("Square Root of 16 is", c**0.5); // here c**0.5 means c to the power 0.5 which means c to the power  1/5 
+                                             // sqaure root of c means c to the power 1/2 and 1/2 = 0.5
