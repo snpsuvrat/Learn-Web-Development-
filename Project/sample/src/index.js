132b95e108fb6fabdf2bcf4120443d6d934f1a30
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-11-23 22:52:17 +0530

    hello react

--- Project/sample/src/index.js
+++ Project/sample/src/index.js
@@ -1,5 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-ReactDOM.render(<h1> hello world!</h1>,
+ReactDOM.render(<h1> hello world!!</h1>,
   document.getElementById('root'));
