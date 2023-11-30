926ced2dbb0b3f8dcf0ed4638fa8bb69e69a186e
snpsuvrat <123564951+snpsuvrat@users.noreply.github.com>
2023-11-30 16:22:21 +0000

    ConditionalOperator

---
+++ JS MASAI Learn/4.ConditionalOperator.js
@@ -0,0 +1,22 @@
+if(True){
+  console.log("If Statement");
+}
+
+if(false)
+{
+  //this statement will not executed as the condition is false
+}
+else{
+  console.log("Else Statement");
+}
+
+if(false){
+  //statement will not be executed
+}
+else if(true){
+  console.log("Else If Statement")
+}
+else
+{
+  console.log("Else Statement");
+}
