e7945d5e42cafc3a82bfebb4d86a267eae08a51f
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-11-30 22:09:01 +0530

    ConditionalStatement

--- JS MASAI Learn/4.ConditionalOperator.js
+++ JS MASAI Learn/4.ConditionalOperator.js
@@ -1,7 +1,9 @@
-if(True){
+//If Statement
+if(true){
   console.log("If Statement");
 }
 
+// If-Else Statement
 if(false)
 {
   //this statement will not executed as the condition is false
@@ -10,13 +12,30 @@ else{
   console.log("Else Statement");
 }
 
+//If-ElseIf-Else Statement
 if(false){
   //statement will not be executed
 }
 else if(true){
-  console.log("Else If Statement")
+  console.log("Else If Statement");
 }
 else
 {
   console.log("Else Statement");
+}
+
+//Nested If Else
+
+if(true)
+{
+  if(false)
+  {
+    console.log("Inner If");
+  }
+  else{
+    console.log("Inner Else");
+  }
+}
+else{
+  console.log("Outer Else");
 }
