6fe9b7c805d6420333605337d56896e4b620a2d4
snpsuvrat <suvrat.pandey.9@gmail.com>
2024-01-05 23:55:51 +0530

    hello

--- wad/WAD MCA Asgn/ass1.js
+++ wad/WAD MCA Asgn/ass1.js
@@ -2,3 +2,8 @@ function time(x)
 {
     document.write("Hi There! Good "+ x);
 }
+let userResponse = parseInt (prompt("Please enter no. 1:"));
+let userResponse2 = parseInt (prompt("Please enter no. 1:"));
+let sum=userResponse+userResponse2;
+alert(sum);
+confirm("sum is correct :");
